b7bccf327fd86c101cc8212bfd9aec1543bafd8a
formidablechief_27 <dhrumilstarwars@gmail.com>
2025-01-10 20:56:26 +0530

    00

--- src/main/java/com/example/gym/controller/LoginController.java
+++ src/main/java/com/example/gym/controller/LoginController.java
@@ -3,16 +3,20 @@ package com.example.gym.controller;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.gym.model.User;
+import com.example.gym.repository.DataCache;
 import com.example.gym.repository.UserRepository;
 
 @Controller
@@ -20,6 +24,8 @@ public class LoginController {
 	
 	@Autowired
     private UserRepository userRepository;
+	@Autowired
+	JavaMailSender mailSender;
 	
 	@Autowired
 	LoginController(UserRepository u) {
@@ -83,11 +89,54 @@ public class LoginController {
 	    }
 	}
 
+	@PostMapping("/otp")
+	public ResponseEntity<Map<String, String>> otp(@RequestBody Map<String, Object> registerRequest) {
+        String username = ((String) registerRequest.get("username")).trim();
+        String email = ((String) registerRequest.get("email")).trim();
+        String pass = (String) registerRequest.get("password");
+        int otp = Integer.parseInt(((String) registerRequest.get("username")).trim());
+        long password = 0;
+        
+        for(int i=0;i<pass.length();i++) {
+        	int l = pass.charAt(i);
+        	password = mul(password, 256);
+        	password = add(password, l);
+        }
+        
+        if(DataCache.otp.containsKey(email)) {
+        	int num = DataCache.otp.get(email);
+        	DataCache.otp.remove(email);
+        	if(num != otp) {
+        		send(email);
+        		Map<String, String> response = new HashMap<>();
+                response.put("message", "Wrong OTP Entered");
+                return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        	}
+        	else {
+        		User user = new User();
+                user.setUsername(username);
+                user.setEmail(email);
+                user.setPassword(password);
+                user.setSecret(0L);
+                userRepository.save(user);
+                Map<String, String> response = new HashMap<>();
+                response.put("message", "Done");
+                response.put("username", user.getUsername());
+	            return new ResponseEntity<>(response, HttpStatus.OK);
+        	}
+        }
+        else {
+    		Map<String, String> response = new HashMap<>();
+            response.put("message", "Some issue, Please try Registering Again");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+
+    }
 
 	@PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody Map<String, Object> registerRequest) {
-        String username = (String) registerRequest.get("username");
-        String email = (String) registerRequest.get("email");
+    public ResponseEntity<Map<String, String>> register(@RequestBody Map<String, Object> registerRequest) {
+        String username = ((String) registerRequest.get("username")).trim();
+        String email = ((String) registerRequest.get("email")).trim();
         String pass = (String) registerRequest.get("password");
         long password = 0;
         
@@ -97,23 +146,41 @@ public class LoginController {
         	password = add(password, l);
         }
 
-        if (userRepository.findByUsername(username).isPresent()) {
-            return new ResponseEntity<>("Username already exists", HttpStatus.BAD_REQUEST);
+        if (userRepository.findByUsername(username).isPresent() || userRepository.findByEmail(email).isPresent()) {
+        	Map<String, String> response = new HashMap<>();
+            response.put("message", "User already exists");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
         }
-
-        if (userRepository.findByEmail(email).isPresent()) {
-            return new ResponseEntity<>("Email already exists", HttpStatus.BAD_REQUEST);
-        }
-
+        
         User user = new User();
         user.setUsername(username);
         user.setEmail(email);
         user.setPassword(password);
         user.setSecret(0L);
+        
+        send(email);
 
-        userRepository.save(user);
-        return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "OTP");
+        
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
+	
+	public void send(String email) {
+		Random rand = new Random();
+		int n = 0;
+        for(int i=0;i<6;i++) {
+        	int num = rand.nextInt(10);
+        	n *= 10;
+        	n += num;
+        }
+		DataCache.otp.put(email, n);
+        SimpleMailMessage message = new SimpleMailMessage();
+    	message.setTo(email);
+    	message.setSubject("Welcome to Gym App");
+    	message.setText("Confirm your registration to the app with OTP = " + n);
+    	mailSender.send(message);
+	}
 	
 	long mod = (long)1e18;
 	long add(long a, long b) {return (((a + mod) % mod + (b + mod) % mod) % mod);}
---
+++ src/main/java/com/example/gym/repository/DataCache.java
@@ -0,0 +1,11 @@
+package com.example.gym.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.gym.model.User;
+
+public class DataCache {
+	
+	public static Map<String, Integer> otp = new HashMap<>();
+}
